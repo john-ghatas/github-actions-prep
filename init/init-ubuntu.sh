adbd8ca666ca8007636bf9fefb7d90c7d96842f7
John Ghatas <johnghatas97@hotmail.com>
2021-02-06 17:39:42 +0100

    feat: Add Node install to init script

--- init/init-ubuntu.sh
+++ init/init-ubuntu.sh
@@ -9,7 +9,12 @@ sudo ln -snf /usr/bin/pip3 /usr/bin/pip
 sudo pip install docker-compose
 
 # Install ClamAV
-sudo apt-get install clamav clamav-daemon
+sudo apt-get install clamav clamav-daemon -y
+
+# Install Node.js v14
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt-get install nodejs -y
+sudo npm i -g npm@latest
 
 # Create group and add CI user with appriopiate permissions
 sudo groupadd devs # Create a group to assign the permissions to

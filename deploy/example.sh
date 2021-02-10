921ed2795a82334b0cd9821517b91c88e63a6dda
John Ghatas <johnghatas97@hotmail.com>
2021-02-10 20:02:30 +0100

    feat: Add example deploy script

---
+++ deploy/example.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ROOT_LOCATION=$1
+RELEASES_FOLDER=$ROOT_LOCATION/releases
+WEBSITE_LOCATION=$RELEASES_FOLDER/$2
+
+set_permissions() {
+    cd $ROOT_LOCATION
+    chown -R :devs $ROOT_LOCATION/
+}
+
+echo "========== Setting permissions on $ROOT_LOCATION"
+set_permissions
+
+echo "========== Creating build in $WEBSITE_LOCATION"
+cd $WEBSITE_LOCATION
+npm ci  --silent
+npm run build --silent
+
+echo "-------------- Deploying main link"
+ln -snf $WEBSITE_LOCATION/build $ROOT_LOCATION/current
+
+echo "-------------- Deploying backup link"
+ln -snf $WEBSITE_LOCATION/build $ROOT_LOCATION/backup
+
+echo "========== Setting permissions after finalizing deployment on $ROOT_LOCATION"
+set_permissions
+
+echo "========== Cleaning up" 
+cd $RELEASES_FOLDER
+ls -lt | tail -n +4 | xargs rm -rf
